9f8bdfe77e79518feafbb9119e0883ddfdc4267e
Vinicius Rufine <vsrufine@gmail.com>
2019-07-27 23:51:53 -0300

    datatypes

--- tutorial/src/tutorial/functions.clj
+++ tutorial/src/tutorial/functions.clj
@@ -16,3 +16,25 @@
 (defn increment_set
   [x]
   (map increment x))
+
+(defn DataTypes []
+  (def a 1)
+  (def b 1.25)
+  (def c 1.25e-12)
+  (def d 0xfbfc)
+  (def e nil)
+  (def f true)
+  (def g "String")
+  (def h 'customkey)
+
+  (println a)
+  (println b)
+  (println c)
+  (println d)
+  (println e)
+  (println f)
+  (println g)
+  (println h)
+)
+
+(DataTypes)
