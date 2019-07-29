be5b4e2d4533664e3d8d3d8efc6a90744b312a35
Vinicius Rufine <vsrufine@gmail.com>
2019-07-28 22:18:35 -0300

    destructuring

---
+++ tutorial/src/tutorial/destructuring.clj
@@ -0,0 +1,14 @@
+(ns tutorial.destructuring)
+
+(defn Destruct
+  []
+  (def myVect [1 2 3 4])
+  (let [[a b c] myVect] (println a b c))
+  (let [[a b & rest] myVect] (println a b rest))
+
+  (def myMap {'name "John" 'lastname "Smith"})
+  (let [{a 'name b 'lastname} myMap] (println a b))
+  (let [{a 'name b 'lastname c 'fullName} myMap] (println a b c))
+  )
+
+(Destruct)
